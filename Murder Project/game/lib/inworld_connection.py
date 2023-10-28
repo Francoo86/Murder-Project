0a36dc9d02a9302b416a19d2d50a3319711ee0e9
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2023-10-28 12:27:54 -0300

    added support to goals
    also the class now supports multiple instances for teammates.

--- Murder Project/game/lib/inworld_connection.py
+++ Murder Project/game/lib/inworld_connection.py
@@ -18,26 +18,30 @@ load_dotenv(os.path.join(GAME_PATH, ".env"))
 # this data is important for the game api.
 KEY = os.getenv("API_KEY")
 WORKSPACE_PATH = os.getenv("WORKSPACE_PATH")
-HEADERS = {"Content-Type": "application/json", "authorization": KEY}
 
 # key for saving the session.
 GRPC_METADATA = "Grpc-Metadata-session-id"
 
-# we need to reinvent the wheel rip.
+# TODO: Allow for 4 sessions.
 class OpenAPIClient:
-    # key should be defined.
     session = Session()
     rest_url = os.getenv("API_URL")
-    headers = copydict(HEADERS)
-    
-        # we require the environment things.
+
+    # we require the environment things.
     def __init__(self, key : str = KEY, workspace : str = WORKSPACE_PATH) -> None:
+        # headers don't need to be static, as they be more prone to change.
+        self.headers = {"Content-Type": "application/json"}
+        self.set_auth_data(key, workspace)
+    
+    # para los panas
+    def set_auth_data(self, key : str, workspace : str):
         self.key = key
         self.workspace = workspace
+        self.headers["authorization"] = key
 
-    def __connect_to_endpoint(self, endpoint : str, delay : int = 0, method : str = "post", data : dict = None, **kwargs):
-        full_path = f"{self.rest_url}{WORKSPACE_PATH}{endpoint}"
-        
+    def __call_api(self, endpoint : str, delay : int = 0, method : str = "post", data : dict = None, **kwargs):
+        full_path = f"{self.rest_url}{self.workspace}{endpoint}"
+
         # connect to endpoint and thats it.
         connection = initialize_connection(self.session, full_path, 
                 method, delay, json=data, headers=self.headers, **kwargs)
@@ -47,16 +51,24 @@ class OpenAPIClient:
         
         return connection.json()
     
-    def request_character_session(self, character : str, ply_data : dict):
-        session_endpoint = f'/characters/{character}:openSession'
-        return self.__connect_to_endpoint(session_endpoint, data=ply_data)
-    
-    def send_prompt(self, sess_id : int, player_id : int, text : str):
-        prompt_endpoint = f'/sessions/{sess_id}/sessionCharacters/{player_id}:sendText'
-        
+    # this is mostly for sessions.
+    def __call_with_grpc(self, endpoint : str, sess_id : str, data : dict):
         # HACK: Add key then remove as this func requires this.
         self.headers[GRPC_METADATA] = sess_id
-        json = self.__connect_to_endpoint(prompt_endpoint, data={"text": text})
+        json = self.__call_api(endpoint, data=data)
         self.headers.pop(GRPC_METADATA)
         
         return json
+    
+    def request_character_session(self, character : str, ply_data : dict):
+        session_endpoint = f'/characters/{character}:openSession'
+        return self.__call_api(session_endpoint, data=ply_data)
+    
+    def send_prompt(self, sess_id : str, player_id : int, text : str):
+        prompt_endpoint = f'/sessions/{sess_id}/sessionCharacters/{player_id}:sendText'
+        return self.__call_with_grpc(prompt_endpoint, sess_id, {"text" : text})
+    
+    def send_goal(self, sess_id : str, player_id : str, trigger_name : str):
+        goal_endpoint = f"/sessions/{sess_id}/sessionCharacters/{player_id}:sendTrigger"
+        return self.__call_with_grpc(goal_endpoint, sess_id, 
+                                    {"triggerEvent": { "trigger":f"{self.workspace}/triggers/{trigger_name}"}})
