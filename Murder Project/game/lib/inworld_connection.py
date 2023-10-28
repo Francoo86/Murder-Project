741f3e2a974f1ed140ed37b233fcfe013b6a6d2a
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2023-10-28 03:47:36 -0300

    proto class OpenAPIClient

--- Murder Project/game/lib/inworld_connection.py
+++ Murder Project/game/lib/inworld_connection.py
@@ -24,15 +24,47 @@ HEADERS = {"Content-Type": "application/json", "authorization": KEY}
 GRPC_METADATA = "Grpc-Metadata-session-id"
 
 # SHARES THE SAME WORKSPACE PATH.
-FULL_SERVICE = urljoin(os.getenv("API_URL"), WORKSPACE_PATH)
+FULL_SERVICE = urljoin(WORKSPACE_PATH)
 
 
 # we need to reinvent the wheel rip.
-class APIClient:
-    def __init__(self) -> None:
-        self.key : str
-        self.path : str
+class OpenAPIClient:
+    # key should be defined.
+    session = Session()
+    rest_url = os.getenv("API_URL")
+    headers = copydict(HEADERS)
 
+    def __connect_to_endpoint(self, endpoint : str, delay : int = 0, method : str = "post", data : dict = None, **kwargs):
+        full_path = f"{self.rest_url}/{WORKSPACE_PATH}{endpoint}"
+        
+        # connect to endpoint and thats it.
+        connection = initialize_connection(self.session, full_path, 
+                method, delay, json=data, headers=self.headers, **kwargs)
+
+        if not connection:
+            raise Exception(f"Can't connect to {full_path}")
+        
+        return connection.json()
+
+    # we require the environment things.
+    def __init__(self, key : str = KEY, workspace : str = WORKSPACE_PATH) -> None:
+        self.key = key
+        self.workspace = workspace
+    
+    def request_character_session(self, character : str, ply_data : dict):
+        session_endpoint = f'/characters/{character}:openSession'
+        return self.__connect_to_endpoint(session_endpoint, data=ply_data)
+    
+    def request_prompt(self, sess_id : int, player_id : int, text : str):
+        prompt_endpoint = f'/sessions/{sess_id}/sessionCharacters/{player_id}:sendText'
+        
+        # HACK: Add key then remove as this func requires this.
+        self.headers[GRPC_METADATA] = sess_id
+        json = self.__connect_to_endpoint(prompt_endpoint, data={"text": text})
+        self.headers.pop(GRPC_METADATA)
+        
+        return json
+        
 class AbstractConnection(ABC):
     # in the case we use get we change it.
     # static session.
