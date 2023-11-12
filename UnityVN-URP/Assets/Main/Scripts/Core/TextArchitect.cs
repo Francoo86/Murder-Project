5395080a91c21aa837bbf9cd5602d3272bac2df2
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2023-11-12 10:52:31 -0300

    added coroutines to TextArchitect

--- UnityVN-URP/Assets/Main/Scripts/Core/TextArchitect.cs
+++ UnityVN-URP/Assets/Main/Scripts/Core/TextArchitect.cs
@@ -25,6 +25,66 @@ public class TextArchitect
     private const float textSpeed = 1;
     private float speedMultiplier = 1;
 
+    public bool veryFastText = false;
+
+    public int charsPerCycle { get { return speed <= 2f ? characterMul : speed <= 2.5f ? characterMul * 2 : characterMul * 3; } }
+    private int characterMul = 1;
+
+    public TextArchitect(TextMeshProUGUI tmpro_ui) {
+        this.tmpro_ui = tmpro_ui;
+    }
+
+    public TextArchitect(TextMeshPro tmpro_world) {
+        this.tmpro_world = tmpro_world;
+    }
+
+    //Coroutine text building.
+    private Coroutine StartTextCoroutine(string text, string oldText = "") {
+        preText = oldText;
+        targetText = text;
+
+        Stop();
+
+        buildProcess = tmpro.StartCoroutine(Building());
+        return buildProcess;
+    }
+    public Coroutine Build(string text) {
+        return StartTextCoroutine(text);
+        /*
+        preText = "";
+        targetText = text;
+
+        Stop();
+
+        buildProcess = tmpro.StartCoroutine(Building());
+        return buildProcess;*/
+    }
+
+    public Coroutine Append(string text)
+    {
+        /*
+        preText = tmpro.text;
+        targetText = text;
+
+        Stop();
+
+        buildProcess = tmpro.StartCoroutine(Building());
+        return buildProcess;*/
+        return StartTextCoroutine(text, tmpro.text);
+    }
 
 
+    public Coroutine buildProcess = null;
+    public bool isBuilding => buildProcess != null;
+
+    public void Stop() {
+        if (!isBuilding) return;
+
+        tmpro.StopCoroutine(buildProcess);
+        buildProcess = null;
+    }
+
+    IEnumerator Building() {
+        yield return null;
+    }
 }
