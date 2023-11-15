ba3f113e0ec136b34ff0621a186397ac6833451b
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2023-11-15 02:28:57 -0300

    added fade to text

--- UnityVN-URP/Assets/Main/Scripts/Core/TextArchitect.cs
+++ UnityVN-URP/Assets/Main/Scripts/Core/TextArchitect.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using UnityEngine;
 using TMPro;
 using System;
+using System.Globalization;
 
 public class TextArchitect
 {
@@ -16,7 +17,9 @@ public class TextArchitect
     public string currentText => tmpro.text;
     public string targetText { get; private set; } = "";
     public string preText { get; private set; } = "";
-    //private int preTextLength = 0;
+
+    //Usado más para los fades.
+    private int preTextLength = 0;
 
     public string fullTargetText => preText + targetText;
     public enum BuildMethod { instant, typewriter, fade};
@@ -34,7 +37,11 @@ public class TextArchitect
     public int charsPerCycle { get { return speed <= 2f ? characterMul : speed <= 2.5f ? characterMul * 2 : characterMul * 3; } }
     private int characterMul = 1;
 
-    public TextArchitect(TextMeshProUGUI tmpro_ui) {
+    public float GetProperTextSpeed() { 
+        return charsPerCycle* (shouldSpeedUp? FASTER_TEXT_SPEED : 1);
+    }
+
+public TextArchitect(TextMeshProUGUI tmpro_ui) {
         this.tmpro_ui = tmpro_ui;
     }
 
@@ -141,6 +148,20 @@ public class TextArchitect
         }
     }
 
+    public void ForceComplete() {
+        switch (buildMethod) { 
+            case BuildMethod.typewriter:
+                tmpro.maxVisibleCharacters = tmpro.textInfo.characterCount;
+                break;
+            case BuildMethod.fade:
+                tmpro.ForceMeshUpdate();
+                break;
+        }
+
+        Stop();
+        OnComplete();
+    }
+
     //TODO: Create a new class that holds this functionality.
     private void Prepare_Instant() {
         //Reinciar color.
@@ -168,20 +189,107 @@ public class TextArchitect
         tmpro.ForceMeshUpdate();
     }
 
+    /// <summary>
+    /// Prepara al texto para que pueda hacer fade. (Efecto visual).
+    /// </summary>
     private void Prepare_Fade()
     {
+        tmpro.text = preText;
+        if (preText != "") { 
+            tmpro.ForceMeshUpdate();
+            preTextLength = tmpro.textInfo.characterCount;
+        }
+        else
+            preTextLength = 0;
 
+        tmpro.text += targetText;
+        tmpro.maxVisibleCharacters = int.MaxValue;
+        tmpro.ForceMeshUpdate();
+
+        TMP_TextInfo textInfo = tmpro.textInfo;
+
+        Color visibleCol = new Color(textColor.r, textColor.g, textColor.b, 1);
+        Color hiddenCol = new Color(textColor.r, textColor.g, textColor.b, 0);
+
+        //Colores que se manejan desde 0-255.
+        Color32[] vertexCols = textInfo.meshInfo[textInfo.characterInfo[0].materialReferenceIndex].colors32;
+
+        for (int i = 0; i < textInfo.characterCount; i++) { 
+            TMP_CharacterInfo charInfo = textInfo.characterInfo[i];
+
+            if(!charInfo.isVisible) { 
+                continue;
+            }
+
+            if (i < preTextLength)
+            {
+                for (int j = 0; j < 4; j++)
+                    vertexCols[charInfo.index + j] = visibleCol;
+            }
+            else {
+                for (int j = 0; j < 4; j++)
+                    vertexCols[charInfo.index + j] = hiddenCol;
+
+            }
+        }
+
+        //Actualizar las referencias del color del texto.
+        tmpro.UpdateVertexData(TMP_VertexDataUpdateFlags.Colors32);
     }
 
     private IEnumerator Build_TypeWriter() {
         while (tmpro.maxVisibleCharacters < tmpro.textInfo.characterCount) {
-            tmpro.maxVisibleCharacters += charsPerCycle * (shouldSpeedUp ? FASTER_TEXT_SPEED : 1);
+            tmpro.maxVisibleCharacters += (int)GetProperTextSpeed();
             yield return new WaitForSeconds(BASE_TEXT_SPEED / speed);
         }
         //yield return null;
     }
 
     private IEnumerator Build_Fade() {
-        yield return null;
+        //TODO: HIPER-REFACTOR.
+        int minRange = preTextLength;
+        int maxRange = minRange + 1;
+        int alphaThreshold = 15;
+
+        TMP_TextInfo textInfo = tmpro.textInfo;
+        Color32[] vertexColors = textInfo.meshInfo[textInfo.characterInfo[0].materialReferenceIndex].colors32;
+        float[] alphas = new float[textInfo.characterCount];
+
+
+        while (true) {
+            for (int i = minRange; i < maxRange; i++) {
+                TMP_CharacterInfo charInfo = textInfo.characterInfo[i];
+                if(!charInfo.isVisible) continue;
+
+                int vertexIndex = textInfo.characterInfo[i].vertexIndex;
+                //Hacer una especie de interpolación lineal, que le daría una transición más bonita.
+                alphas[i] = Mathf.MoveTowards(alphas[i], 255, GetProperTextSpeed() * 4f);
+
+                for (int j = 0; j < 4; j++) {
+                    vertexColors[charInfo.vertexIndex + j].a = (byte)alphas[i];
+                }
+
+                if (alphas[i] >= 255) {
+                    minRange++;
+                }
+
+                tmpro.UpdateVertexData(TMP_VertexDataUpdateFlags.Colors32);
+
+                //Revisa si el caracter es invisible o si se excedio del alpha (no hay transparencia).
+                bool isLastCharInvisible = !textInfo.characterInfo[maxRange - 1].isVisible;
+
+                if (alphas[maxRange - 1] > alphaThreshold || isLastCharInvisible) {
+                    if (maxRange < textInfo.characterCount)
+                    {
+                        maxRange++;
+                    }
+                    else if (alphas[maxRange - 1] >= 255 || isLastCharInvisible)
+                        break;
+                }
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
+        //yield return null;
     }
 }
--- UnityVN-URP/Assets/_TESTING/Scripts/TestingArchitect.cs
+++ UnityVN-URP/Assets/_TESTING/Scripts/TestingArchitect.cs
@@ -9,6 +9,8 @@ namespace TESTING {
         DialogController dc;
         TextArchitect arch;
 
+        public TextArchitect.BuildMethod bm = TextArchitect.BuildMethod.instant;
+
         string[] lines = new string[5] {
             "Hola me presento",
             "Lorem Ipsum bla bla",
@@ -22,15 +24,34 @@ namespace TESTING {
         {
             dc = DialogController.Instance;
             arch = new TextArchitect(dc.dialog.dialogText);
-            arch.buildMethod = TextArchitect.BuildMethod.typewriter;
+            arch.buildMethod = TextArchitect.BuildMethod.fade;
         }
 
         // Update is called once per frame
         void Update()
         {
+            if (bm != arch.buildMethod) {
+                arch.buildMethod = bm;
+                arch.Stop();
+            }
+
+            if (Input.GetKeyDown("s"))
+                arch.Stop();
+
             // Debug.Log(string.Format("Is pressing A Key: {0}", Input.GetKey(KeyCode.A)));
-            if (Input.GetKeyDown("space")) {
-                arch.Build(lines[UnityEngine.Random.Range(0, lines.Length)]);
+            if (Input.GetKeyDown("k")) {
+                //arch.Build(lines[UnityEngine.Random.Range(0, lines.Length)]);
+                if (arch.isBuilding)
+                {
+                    if (!arch.shouldSpeedUp)
+                    {
+                        arch.shouldSpeedUp = true;
+                    }
+                    else
+                        arch.ForceComplete();
+                }
+                else
+                    arch.Build(lines[UnityEngine.Random.Range(0, lines.Length)]);
             }
             else if (Input.GetKeyDown("a"))
             {
