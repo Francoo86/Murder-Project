2533e8556ae1cd66e4adbcfa2708b510d4a02aff
PieroTandaypanValle <115677852+PieroTandaypanValle@users.noreply.github.com>
2023-11-24 14:32:28 -0300

    Update
    -nueva carpeta de Character
    -nuevo script CharacterSpriteLayer

---
+++ UnityVN-URP/Assets/Main/Scripts/Core/Characters/CharacterSpriteLayer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CHARACTERS
+{
+
+    public class CharacterSpriteLayer
+    {
+        private CharacterManager characterManager => characterManager.instance;
+
+        public int layer { get; private set; } = 0;
+
+        public Image renderer { get; private set; } = null;
+
+        public CanvasGroup renderCG => renderer.GetComponent<CanvasGroup>();
+
+        private List<CanvasGroup> oldRenderers = new List<CanvasGroup> ();
+
+        public CharacterSpriteLayer (Image defaultRenderer, int layer = 0)
+        {
+            renderer = defaultRenderer;
+            this.layer = layer;
+        }
+    }
+}
