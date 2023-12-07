5bc871c13c0ff8d624e0ec282a8b75222ecdf859
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2023-12-07 00:59:18 -0300

    approach to new input system

--- UnityVN-URP/Assets/Main/Scripts/Core/UserControls/PlayerInputManager.cs
+++ UnityVN-URP/Assets/Main/Scripts/Core/UserControls/PlayerInputManager.cs
@@ -1,27 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInputManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private PlayerInput input;
+    //Estas acciones son asignadas en un archivo y en un cambio de escena PODRIAN apuntar a referencias nulas.
+    //OJO.
+    private List<(InputAction action, Action<InputAction.CallbackContext> command)> actions = new List<(InputAction action, Action<InputAction.CallbackContext>)> ();
+    void Awake()
     {
-        
+        input = GetComponent<PlayerInput>();
+        actions.Add((input.actions["Next"], PromptAdvance));
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Funcion que se ejecuta al momento de cambiar escena.
+    /// </summary>
+    private void OnEnable()
     {
-        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Return))
-        {
-            Debug.Log("En verdad estoy llamando esto?");
-            PromptAdvance();
-        }
+        foreach(var inputAct in actions)
+            inputAct.action.performed += inputAct.command;
     }
 
-    //Minuto 42
-    public void PromptAdvance() {
+    private void OnDisable()
+    {
+        foreach (var inputAct in actions)
+            inputAct.action.performed -= inputAct.command;
+    }
+    public void PromptAdvance(InputAction.CallbackContext c) {
         DialogController.Instance.OnUserPrompt_Next();
     }
 }
