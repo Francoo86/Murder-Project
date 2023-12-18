4a0e6883730d2e0204dcf9815a02c590ad474af4
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2023-12-18 01:40:54 -0300

    document CommandDB

--- UnityVN-URP/Assets/Main/Scripts/Core/Commands/Database/CommandDB.cs
+++ UnityVN-URP/Assets/Main/Scripts/Core/Commands/Database/CommandDB.cs
@@ -6,7 +6,19 @@ using UnityEngine;
 public class CommandDB
 {
     private Dictionary<string, Delegate> database = new Dictionary<string, Delegate>();
+    /// <summary>
+    /// Checks if the command name is on the database.
+    /// </summary>
+    /// <param name="commandName">The name of command</param>
+    /// <returns>Wether the command exists or not.</returns>
     public bool HasCommand(string commandName) => database.ContainsKey(commandName);
+    /// <summary>
+    /// Adds a command into the command database with its associate function (delegate).
+    /// An analogy of this is Counter-Strike, where you put commands like sv_cheats and you put 1, and that internally calls a function
+    /// so this is the same.
+    /// </summary>
+    /// <param name="commandName">The command name.</param>
+    /// <param name="command">The function that the command will execute.</param>
     public void AddCommand(string commandName, Delegate command)
     {
         commandName = commandName.ToLower();
@@ -14,6 +26,11 @@ public class CommandDB
         else Debug.LogWarning($"{commandName} command already exists on the database!");
     }
     
+    /// <summary>
+    /// Gets the command function with its name.
+    /// </summary>
+    /// <param name="commandName">Command name</param>
+    /// <returns>The function that holds the command name.</returns>
     public Delegate GetCommand(string commandName)
     {
         commandName = commandName.ToLower();
