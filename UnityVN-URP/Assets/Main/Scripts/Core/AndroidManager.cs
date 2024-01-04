15c2ab96288198ee641f277412b4d09736b640b6
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-01-04 02:20:30 -0300

    document AndroidManager

--- UnityVN-URP/Assets/Main/Scripts/Core/AndroidManager.cs
+++ UnityVN-URP/Assets/Main/Scripts/Core/AndroidManager.cs
@@ -7,9 +7,10 @@ using History;
 /// Adapter to some things in android.
 /// </summary>
 public class AndroidManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    
+{   
+    /// <summary>
+    /// Initializes the manager and hides all PC related GUI elements, and shows the phone ones.
+    /// </summary>
     private void Start()
     {
         //Make this game for android i guess.
@@ -33,16 +34,25 @@ public class AndroidManager : MonoBehaviour
 
     private HistoryManager HManager => HistoryManager.Instance;
 
+    /// <summary>
+    /// Goes back on the history, like the previous saved state (if any).
+    /// </summary>
     public void GoBack()
     {
         HManager.GoBack();
     }
 
+    /// <summary>
+    /// Goes forward on the history, only if the last state was saved.
+    /// </summary>
     public void GoForward()
     {
         HManager.GoFoward();
     }
 
+    /// <summary>
+    /// Opens the history log with the history button.
+    /// </summary>
     public void OnHistoryLogOpen()
     {
         var Log = HManager.logManager;
@@ -50,6 +60,9 @@ public class AndroidManager : MonoBehaviour
             Log.Open();
     }
 
+    /// <summary>
+    /// Closes the log manager.
+    /// </summary>
     public void OnHistoryLogClose()
     {
         var Log = HManager.logManager;
